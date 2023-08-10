2c586dbd36d787af04c12bf15a2f3e7877ac3fcd
B Abhishek <84638277+paulabhishek787@users.noreply.github.com>
2023-08-10 17:07:39 +0530

    DqCheckMethods.scala

--- src/main/scala/com/target_ready/data/pipeline/dqCheck/DqCheckMethods.scala
+++ src/main/scala/com/target_ready/data/pipeline/dqCheck/DqCheckMethods.scala
@@ -10,6 +10,7 @@ import org.apache.spark.sql.expressions.Window
  * Ignore for now (Changes have to be made)
  *  ============================================================================================================== */
 
+// Define a set of Data Quality (DQ) check methods
 object DqCheckMethods {
 
   /** ===============================================================================================================
@@ -20,13 +21,20 @@ object DqCheckMethods {
    * @return              true: if no null value is found and vice-versa
    * ============================================================================================================= */
   def dqNullCheck(df: DataFrame, keyColumns: Seq[String]): Boolean = {
-
+    // Create a sequence of Column objects representing the key columns
     val columnNames: Seq[Column] = keyColumns.map(c => col(c))
+    
+    // Define a condition to check for null values, empty strings, "NULL", and "null"
     val condition: Column = columnNames.map(c => c.isNull || c === "" || c.contains("NULL") || c.contains("null")).reduce(_ || _)
+       
+    // Add a "nullFlag" column indicating if any of the key columns have null values 
     val dfCheckNullKeyRows: DataFrame = df.withColumn("nullFlag", when(condition, value = true).otherwise(value = false))
-
+    
+    // Filter the DataFrame to only include rows with null values
     val nullDf: DataFrame = dfCheckNullKeyRows.filter(dfCheckNullKeyRows("nullFlag") === true)
-    if (nullDf.count() > 0) throw DqNullCheckException("The file contains nulls")
+
+    // If any null values are found, throw a DqNullCheckException
+    if (nullDf.count() > 0) throw DqNullCheckException("The file contains nulls")  
     true
 
   }
@@ -43,10 +51,15 @@ object DqCheckMethods {
    * =============================================================================================================*/
   def DqDuplicateCheck(df: DataFrame, KeyColumns: Seq[String], orderByCol: String): Boolean = {
 
+    // Create a Window specification for partitioning and ordering
     val windowSpec = Window.partitionBy(KeyColumns.map(col): _*).orderBy(desc(orderByCol))
+
+    // Add a row number column to identify duplicates based on ordering
     val dfDropDuplicate: DataFrame = df.withColumn(colName = ROW_NUMBER, row_number().over(windowSpec))
       .filter(col(ROW_NUMBER) === 1).drop(ROW_NUMBER)
-
+    
+    // Compare the row counts before and after dropping duplicates
+    // If counts are not equal, throw a DqDupCheckException
     if (df.count() != dfDropDuplicate.count()) throw DqDupCheckException("The file contains duplicate")
     true
   }
